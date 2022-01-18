8d2d7f74b92ad731152283dc92580328c309177a
Matthew Bluteau <matthew.bluteau@gmail.com>
2022-01-18 17:21:55 +0000

    Add Patient, Person, and Observation classes with minor test

--- inflammation/models.py
+++ inflammation/models.py
@@ -54,6 +54,7 @@ def attach_names(data, names):
         patients.append({'name': name, 'data': patient_data})
     return patients
 
+
 def patient_normalise(data):
     """Normalise patient data from a 2D inflammation data array.
 
@@ -66,6 +67,42 @@ def patient_normalise(data):
     normalised[normalised < 0] = 0
     return normalised
 
-# TODO(lesson-design) Add Patient class
+
+class Observation:
+    def __init__(self, day, value):
+        self.day = day
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return self.name
+
+
+class Patient(Person):
+    """A patient in an inflammation study."""
+    def __init__(self, name):
+        super().__init__(name)
+        self.observations = []
+
+    def add_observation(self, value, day=None):
+        if day is None:
+            try:
+                day = self.observations[-1].day + 1
+
+            except IndexError:
+                day = 0
+
+        new_observation = Observation(day, value)
+
+        self.observations.append(new_observation)
+        return new_observation
+
 # TODO(lesson-design) Implement data persistence
 # TODO(lesson-design) Add Doctor class
--- tests/test_patient.py
+++ tests/test_patient.py
@@ -8,3 +8,21 @@ def test_create_patient():
     p = Patient(name=name)
 
     assert p.name == name
+
+
+def test_add_observation():
+    from inflammation.models import Patient
+
+    name = 'Alice'
+    p = Patient(name=name)
+    p.add_observation(value=3, day=0)
+
+    assert (p.observations[0].day, p.observations[0].value) == (0, 3)
+
+
+def test_create_doctor():
+    from inflammation.models import Doctor
+
+    name = 'Bob'
+    d = Doctor(name=name)
+    assert d.name == name
