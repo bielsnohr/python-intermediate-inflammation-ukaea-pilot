cfb2112ab3f05d52b51fadf3867fa813ba26a696
Matthew Bluteau <matthew.bluteau@gmail.com>
2022-05-16 16:14:08 +0100

    Failed attempt to see effect of pooling

--- functional_exercises/pool_sum_of_squares.py
+++ functional_exercises/pool_sum_of_squares.py
@@ -2,13 +2,19 @@ import pytest
 from functools import reduce
 from multiprocessing.pool import Pool
 
+def sum_of_squares(sequence):
+    """The non-parallel version"""
+    squares = [x * x for x in sequence]
+    return reduce(lambda a, b: a + b, squares)
+
 
 def product(x):
     return x * x
 
 
 # TODO this isn't yielding any performance gain. Investigate!
-def sum_of_squares(sequence):
+def pool_sum_of_squares(sequence):
+    """The multiprocessing version"""
     with Pool(processes=6) as pool:
         squares = pool.map(product, sequence)
     return reduce(lambda a, b: a + b, squares)
@@ -20,7 +26,14 @@ def sum_of_squares(sequence):
                          ))
 def test_sum_of_squares(sequence, expected):
     assert sum_of_squares(sequence) == expected
+    assert pool_sum_of_squares(sequence) == expected
 
 
-sequence = [i for i in range(10000)]
-print(sum_of_squares(sequence))
+if __name__ == "__main__":
+    import timeit
+    sequence = [i for i in range(10000)]
+    numb = 1000
+    print(timeit.timeit("sum_of_squares(sequence)", globals=globals(),
+                        number=numb))
+    print(timeit.timeit("pool_sum_of_squares(sequence)", globals=globals(),
+                  number=numb))
